086e505a5cc4482037bf76c73801e44b82796804
tlaaksen12 <87048693+tlaaksen12@users.noreply.github.com>
2021-09-03 10:59:50 +0900

    Update PaymentService.java

--- src/main/java/clothingstore/external/PaymentService.java
+++ src/main/java/clothingstore/external/PaymentService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-@FeignClient(name="payment", url="http://user09-gateway:8080")
+@FeignClient(name="payment", url="${prop.pay.url}")
 // @FeignClient(name="payment", url="${api.url.payment}", fallback = PaymentServiceImpl.class)
 public interface PaymentService {
     @RequestMapping(method= RequestMethod.POST, path="/payments")
